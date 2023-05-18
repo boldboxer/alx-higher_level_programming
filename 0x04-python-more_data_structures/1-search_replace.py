7e2998eca8f42ede0d8a84b05f10a5810dd91109
boldboxer <ebesteinpr@gmail.com>
2023-05-18 00:05:09 -0700

    replaces all occurrences of an element by another in a new lis

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,4 +3,6 @@
 
 
 def search_replace(my_list, search, replace):
+    """ replaces all occurrences of an element by another in a new lis
+    """
     return ([ele if ele != search else replace for ele in my_list])
