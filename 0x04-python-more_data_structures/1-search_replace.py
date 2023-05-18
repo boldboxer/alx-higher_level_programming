f26c649eb2d80846c854104f23b67ffc78fcec6a
boldboxer <ebesteinpr@gmail.com>
2023-05-18 00:03:45 -0700

    replaces all occurrences of an element by another in a new list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,6 +3,4 @@
 
 
 def search_replace(my_list, search, replace):
-    def find_search(element):
-        return element if element != search else replace
-    return list(map(find_search, my_list))
+    return ([ele if ele != search else replace for ele in my_list])
